68909ce24b66e4b10fad096fdcf7833871a5863c
Justin Ricaurte <justin@justinricaurte.com>
2013-03-31 02:38:27 -0700

    added flags to signal that rapido is enabled and fixed the environment variable check

--- lib/rapido/rspec_ext/rspec.rb
+++ lib/rapido/rspec_ext/rspec.rb
@@ -5,13 +5,15 @@ module RSpec
   end
 
   def self.enable_rapido
+    puts "Rapido - enabled"
     @rapido_enabled = true
   end
 
   def self.disable_rapido
+    puts "Rapido - disabled (to enable rapido send RAPIDO=true as an environment parameter with your rspec tests)"
     @rapido_enabled = false
   end
 
 end
 
-ENV['RAPIDO'] == true ? RSpec.enable_rapido : RSpec.disable_rapido
+ENV['RAPIDO'] == "true" ? RSpec.enable_rapido : RSpec.disable_rapido
